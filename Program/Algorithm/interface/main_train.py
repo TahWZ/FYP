7d4b30e5d2665fcf02c39a29177e0672130ec573
wtah0001 <wtah0001@student.monash.edu>
2021-07-13 22:19:02 +0800

    Update training settings window
    Design completed, Functions still in progress

--- Program/Algorithm/interface/main_train.py
+++ Program/Algorithm/interface/main_train.py
@@ -18,8 +18,8 @@ lab_1 = Label(main_train, text="Training/Test")
 lab_2 = Label(main_train, text="Upload dataset")
 lab_3 = Label(main_train, text="Prediction file name")
 lab_4 = Label(main_train, text="Prediction file path")
-lab_2_1 = Label(frame_ud, text="Name")
-lab_2_2 = Label(frame_ud, text="Remove")
+lab_2_1 = Label(frame_ud, text="Name", width = 50, anchor=W, bg="light grey")
+lab_2_2 = Label(frame_ud, text="Remove", width = 20, anchor=W, bg="light grey")
 
 #Button
 '''
@@ -57,23 +57,23 @@ ent_pfp = Entry(main_train, width=20)#Prediction file path
 .grid(row, column, columnspan)
 '''
 #Row 0
-lab_1.grid(row = 0, column = 1)
-ent_tt.grid(row = 0, column = 2)
+lab_1.grid(sticky="W", row = 0, column = 1)
+ent_tt.grid(sticky="W", row = 0, column = 2)
 #Row 1
-lab_2.grid(row = 1, column = 1)
+lab_2.grid(sticky="W", row = 1, column = 1)
 #Row 2
-frame_ud.grid(row = 2, column = 1, padx=10, pady=10)
-lab_2_1.grid(row = 0, column = 1)
-lab_2_2.grid(row = 0, column = 2)
+frame_ud.grid(sticky="W", row = 2, column = 1, padx=10, pady=10)
+lab_2_1.grid(sticky="W", row = 0, column = 1)
+lab_2_2.grid(sticky="W", row = 0, column = 2)
 #Row 3
-but_ud_remove.grid(row = 3, column = 1)
-but_ud_upload.grid(row = 3, column = 2)
+but_ud_remove.grid(row = 3, column = 1, pady = 10)
+but_ud_upload.grid(row = 3, column = 2, pady = 10)
 #Row 4
-lab_3.grid(row = 4, column = 1)
-ent_pfn.grid(row = 4, column = 2)
+lab_3.grid(sticky="W", row = 4, column = 1)
+ent_pfn.grid(sticky="W", row = 4, column = 2)
 #Row 5
-lab_4.grid(row = 5, column = 1)
-ent_pfp.grid(row = 5, column = 2)
+lab_4.grid(sticky="W", row = 5, column = 1)
+ent_pfp.grid(sticky="W", row = 5, column = 2)
 
 #====================( Main )====================
 main_train.grid(row = 0, column = 0)
