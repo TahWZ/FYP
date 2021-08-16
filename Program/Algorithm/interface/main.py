817bf2e4b02b7ce23ec5d5927218b1ba0f77f0f6
wtah0001 <wtah0001@student.monash.edu>
2021-08-16 15:42:42 +0800

    Updated the main algorithm
    Updated main algorithm to have multiple tabs

--- Program/Algorithm/interface/main.py
+++ Program/Algorithm/interface/main.py
@@ -2,10 +2,53 @@ import sys
 from tkinter import *
 #sys.path.append('interface')
 from home import Home
+from about import About
+from tkinter import ttk
 #from PIL import ImageTK, Image
 
 #====================( Main )====================
-root = Tk()
-Home(root)
-root.title('Prediction software')
-root.mainloop()
+class Main:
+    #====================( Functions )====================
+    def __init__(self, root):
+        #====================( Frames )====================
+        self.frame_main = Frame(root)
+        #====================( Notebook )====================
+        self.tabControl = ttk.Notebook(self.frame_main)
+        #====================( Tabs )====================
+        self.get_tabs()
+        #====================( Widgets )====================
+        #Label
+        lab_1 = Label(self.frame_main, text="Logo", bg = "black", fg = "white", width = 80, height = 10)
+        #====================( Display )====================
+        '''
+        @Functions
+        .pack(padx, pady)
+        .grid(row, column, columnspan)
+        '''
+        #Row 0
+        lab_1.pack(fill=BOTH)
+        #End
+        self.tabControl.pack()
+        self.frame_main.pack()
+
+    def get_tabs(self):
+        tab = ttk.Frame(self.tabControl)
+        self.tabControl.add(tab, text='Main algorithm')
+        #Tab 1
+        frame = Frame(tab)
+        Home(frame)
+        frame.pack()
+        tab = ttk.Frame(self.tabControl)
+        self.tabControl.add(tab, text='About us')
+        #Tab 2
+        frame = Frame(tab)
+        About(frame)
+        frame.pack(expand=True)
+
+#====================( Main )====================
+if __name__=='__main__':
+    root = Tk()
+    Main(root)
+    root.title('Prediction software')
+    root.mainloop()
+
