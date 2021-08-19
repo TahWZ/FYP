60af7ea9d1fc4c6051b6a9fb27725cd7c71217d4
Earhead <crhawkeye8018@gmail.com>
2021-08-19 12:33:01 +0800

    Added tab for ensemble models

--- Program/Algorithm/interface/home_pred.py
+++ Program/Algorithm/interface/home_pred.py
@@ -7,8 +7,12 @@ class HomePred():
     def __init__(self, home):
         #====================( Variables )====================
         self.base = []
+        self.ensemble = []
         for _ in range(5):
             self.base.append(IntVar())
+            
+        for _ in range(3):
+            self.ensemble.append(IntVar())    
         #====================( Main Frame )====================
         self.frame_main = LabelFrame(home, text="Prediction settings", padx = 10, pady = 10)
         #====================( Notebook )====================
@@ -20,6 +24,7 @@ class HomePred():
         self.tabControl.add(tab_ensemble, text='Ensemble')
         #====================( Frames )====================
         frame_base = Frame(tab_base)
+        frame_ensemble = Frame(tab_ensemble)
         #====================( Widgets )====================
         #--------------------( Tab 1 )--------------------
         #Label
@@ -36,6 +41,15 @@ class HomePred():
         t1_cb4 = Checkbutton(frame_base, variable=self.base[3])
         t1_cb5 = Checkbutton(frame_base, variable=self.base[4])
         #--------------------( Tab 2 )--------------------
+        #Label
+        t2_lab1 = Label(frame_ensemble, text="Random Forest")
+        t2_lab2 = Label(frame_ensemble, text="Rotation Forest")
+        t2_lab3 = Label(frame_ensemble, text="Voting")
+        
+        #Checkbutton
+        t2_cb1 = Checkbutton(frame_ensemble, variable=self.ensemble[0])
+        t2_cb2 = Checkbutton(frame_ensemble, variable=self.ensemble[1])
+        t2_cb3 = Checkbutton(frame_ensemble, variable=self.ensemble[2])
         #--------------------( Others )--------------------
 
         #====================( Display )====================
@@ -68,6 +82,21 @@ class HomePred():
         #End
         frame_base.grid(row = 0, column = 1)
         #--------------------( Tab 2 )--------------------
+        t2_labt1 = Label(frame_ensemble, text="Base predictors", width = 20, bg = "light grey", anchor = W)
+        t2_labt1.grid(sticky="W", row = 0, column = 1)
+        t2_labt2 = Label(frame_ensemble, text="Select", bg = "light grey", anchor = W)
+        t2_labt2.grid(sticky="W", row = 0, column = 2)
+        #Row 1
+        t2_lab1.grid(sticky="W", row = 1, column = 1)
+        t2_cb1.grid(row = 1, column = 2)
+        #Row 2
+        t2_lab2.grid(sticky="W", row = 2, column = 1)
+        t2_cb2.grid(row = 2, column = 2)
+        #Row 3
+        t2_lab3.grid(sticky="W", row = 3, column = 1)
+        t2_cb3.grid(row = 3, column = 2)
+        #End
+        frame_ensemble.grid(row = 0, column = 1)
         #--------------------( Others )--------------------
         self.tabControl.grid(row = 0, column = 1)
         self.frame_main.pack(side=LEFT, fill=BOTH, pady = 10)
