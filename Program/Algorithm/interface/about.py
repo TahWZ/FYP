e12bfd1d129b1f1abb82f827c89fd0c2044f11b4
Jason <jtoh0003@student.monash.edu>
2021-09-21 14:53:18 +0800

    Added about us pictures

--- Program/Algorithm/interface/about.py
+++ Program/Algorithm/interface/about.py
@@ -1,6 +1,8 @@
 import sys
 from tkinter import *
 import webbrowser
+from PIL import ImageTk, Image
+import os
 
 #from home_train import HomeTrain
 #from home_pred import HomePred
@@ -14,7 +16,6 @@ class About:
         self.frame_main = Frame(root)
         self.desc = "We are a group of 3 students from Monash University that have been tasked with building prediction models for software modules that are capable of handling the problem of dataset imbalance. As such, we have made the decision to develop this program." 
         self.desc2 = "Our aim is to create a program which lets the user upload their desired software datasets, and then give them the option of choosing which prediction models they would like to use against their datasets. Further details about the program will be explained in the next section."
-        self.desc3 = "Before running the algorithm within the program. Users are required to upload at least one of their desired software datasets. Then, they can also select the prediction models they wish to use on their datasets. After that, the program takes them to a screen which lets them decide whether they want to include additional algorithms which optimize the selection of software metrics. Once that is done, the algorithm runs in the background, and then takes the user to the results screen, at which they can choose to view the results of the prediction algorithms in table view as well as chart view."
         #====================( Widgets )====================
         #Label
         #Who we are
@@ -25,13 +26,48 @@ class About:
         top_2 = Label(self.frame_main, text="Objective", font=('Helvetica', 16, 'bold'))
         text_2 = Label(self.frame_main, text=self.desc2, font=('Helvetica', 11), wraplength = 475, justify = LEFT)
 
-        #Summary
-        top_3 = Label(self.frame_main, text="How the Program Works", font=('Helvetica', 16, 'bold'))
-        text_3 = Label(self.frame_main, text=self.desc3, font=('Helvetica', 11), wraplength = 475, justify = LEFT)
-
         #Bind 
         #label.bind("<Button-1>", lambda e:open_url(url))
 
+        # Information
+        names = ['Jason','Tah','Ethan']
+        # emails = ['jtoh0003@student.monash.edu','wtah0001@student.monash.edu','ehor0005@student.monash.edu']
+        emails =['jtoh0003','wtah0001','ehor0005']
+        stickies = ['NW','N','NE']
+
+        # Parent directory of current file
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+
+        # Images
+        # img1 = Image.open(path + '/images/placeholder.png')
+        img1 = Image.open(path + '/images/jason.JPG')
+        resized_image= img1.resize((100,100), Image.ANTIALIAS)
+        img1 = ImageTk.PhotoImage(resized_image)
+        panel1 = Label(self.frame_main, image = img1)
+        panel1.image = img1
+        panel1.grid(row=0,column=0,sticky=NW)
+
+        # img2 = Image.open(path + '/images/placeholder.png')
+        img2 = Image.open(path + '/images/tah.JPG')
+        resized_image= img2.resize((100,100), Image.ANTIALIAS)
+        img2 = ImageTk.PhotoImage(resized_image)
+        panel2 = Label(self.frame_main, image = img2)
+        panel2.image = img2
+        panel2.grid(row=0,column=0,sticky=N)
+
+        # img3 = Image.open(path + '/images/placeholder.png')
+        img3 = Image.open(path + '/images/ethan.JPG')
+        resized_image= img3.resize((100,100), Image.ANTIALIAS)
+        img3 = ImageTk.PhotoImage(resized_image)
+        panel3 = Label(self.frame_main, image = img3)
+        panel3.image = img3
+        panel3.grid(row=0,column=0,sticky=NE)
+
+        for i in range(len(names)):
+            name = Label(self.frame_main, text=names[i], font=('Helvetica', 11),justify = LEFT)
+            email = Label(self.frame_main, text=emails[i], font=('Helvetica', 11),justify = LEFT)
+            name.grid(row=1,column=0,sticky=stickies[i])
+            email.grid(row=2,column=0,sticky=stickies[i])
         #====================( Display )====================
         '''
         @Functions
@@ -39,17 +75,19 @@ class About:
         .grid(row, column, columnspan)
         '''
         #Rows
-        top_1.pack(anchor = 'w')
-        text_1.pack(anchor = 'w')
-        top_2.pack(anchor = 'w')
-        text_2.pack(anchor = 'w')
-        top_3.pack(anchor = 'w')
-        text_3.pack(anchor = 'w')
-        #End
+        # top_1.pack(anchor = 'w')
+        # text_1.pack(anchor = 'w')
+        # top_2.pack(anchor = 'w')
+        # text_2.pack(anchor = 'w')
+        top_1.grid(row=3,column=0,sticky=W)
+        text_1.grid(row=4,column=0,sticky=W)
+        top_2.grid(row=5,column=0,sticky=W)
+        text_2.grid(row=6,column=0,sticky=W)
+        # End
         self.frame_main.pack(expand = True)
 
     def open_url(url):
-       webbrowser.open_new_tab(url)
+        webbrowser.open_new_tab(url)
             
 
 #====================( Main )====================
--- Program/Algorithm/main.py
+++ Program/Algorithm/main.py
@@ -3,10 +3,12 @@ from tkinter import *
 from interface.home import Home
 from interface.about import About
 from interface.report import Report
+from interface.instructions import Instruction
 import interface.login
 from tkinter import ttk
 from tkinter import messagebox
 from PIL import ImageTk, Image
+import os
 
 #====================( Main )====================
 class Main:
@@ -14,7 +16,7 @@ class Main:
     def __init__(self, root):
         #================( Root Reference )================
         self.root = root
-        self.photo = ImageTk.PhotoImage(Image.open("logo.png"))
+        self.photo = ImageTk.PhotoImage(Image.open(os.getcwd()+'/images/logo.png'))
         #====================( Frames )====================
         self.frame_main = Frame(root)
         #====================( Notebook )====================
@@ -58,6 +60,12 @@ class Main:
         self.tabControl.add(tab, text='About us')
         frame = Frame(tab)
         About(frame)
+        frame.pack(expand=True)
+        #Tab 4
+        tab = ttk.Frame(self.tabControl)
+        self.tabControl.add(tab, text='Instructions')
+        frame = Frame(tab)
+        Instruction(frame)
         frame.pack(expand=True)
     
     def logout(self):
